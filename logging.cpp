f6ffdf8b51bbdc314b123673202b6881f789dc79
Viacheslav Germonenko <vyacheslav.germonenko@gmail.com>
2019-04-16 21:32:03 +0300

    logging class now catches no exceptions

--- logging.cpp
+++ logging.cpp
@@ -1,30 +1,29 @@
 #include "logging.hpp"
 #include <iostream>
 
-user* logging::current_callback_data_ = nullptr;
+user* logging_options::current_callback_data_ = nullptr;
 
-user* logging::sign_in()
+user* logging_options::sign_in()
 {
 	connection conn("Taxi.sqlite3");
 	auto user_logging_data = ask_logging_data();
 	auto SQLtext = form_sign_in(user_logging_data);
-	try_execute(conn, SQLtext, sign_in_callback);
+	conn.execute(SQLtext, nullptr, sign_in_callback);
 	return current_callback_data_;
 }
 
-user* logging::sign_up()
+user* logging_options::sign_up()
 {
 	connection conn("Taxi.sqlite3");
 	auto role = register_as();
 	auto user_credentials = ask_credentials();
 	auto user_logging_data = ask_logging_data();	
 	auto SQLtext = form_sign_up(user_logging_data, user_credentials, role);
-	if (try_execute(conn, SQLtext, nullptr) == 0)
-		return new user(user_logging_data, user_credentials, role);
-	return nullptr;
+	conn.execute(SQLtext, nullptr);
+	return new user(user_logging_data, user_credentials, role);
 }
 
-int logging::register_as()
+int logging_options::register_as()
 {
 	std::string reg = "null";
 	while (register_roles.count(reg) == 0)
@@ -36,7 +35,7 @@ int logging::register_as()
 	return register_roles[reg];
 }
 
-credentials logging::ask_credentials()
+credentials logging_options::ask_credentials()
 {
 	credentials credits;
 	std::cout << "Enter your name: ";
@@ -46,7 +45,7 @@ credentials logging::ask_credentials()
 	return credits;
 }
 
-logging_data logging::ask_logging_data()
+logging_data logging_options::ask_logging_data()
 {
 	logging_data data;
 	std::cout << "Enter your login: ";
@@ -56,30 +55,19 @@ logging_data logging::ask_logging_data()
 	return data;
 }
 
-std::string logging::form_sign_in(const logging_data& data)
+std::string logging_options::form_sign_in(const logging_data& data)
 {
 	return "SELECT * FROM users WHERE login = '" + data.login + "' and password = '" + data.password + "'";
 }
 
-std::string logging::form_sign_up(const logging_data& data, const credentials& credits, const int& role)
+std::string logging_options::form_sign_up(const logging_data& data, const credentials& credits, const int& role)
 {
 	return "INSERT INTO users (login, password, name, surname, role) " \
 		    "VALUES ('" +  data.login + "', '" + data.password + "', '" + credits.name + "', '" + credits.surname + "', " + std::to_string(role) + ")";
 
 }
 
-int logging::try_execute(connection& conn, const std::string& SQLtext, callback func)
-{
-	try	{
-		conn.execute(SQLtext, nullptr, func);
-		return 0;
-	} catch (database_exception& exception) {
-		// TODO: do smth with error code
-	}
-	return 1;
-}
-
-int logging::sign_in_callback(void* user_found, int count, char** value, char** column_name)
+int logging_options::sign_in_callback(void* user_found, int count, char** value, char** column_name)
 {
 	logging_data data;
 	credentials credits;
@@ -95,7 +83,7 @@ int logging::sign_in_callback(void* user_found, int count, char** value, char** 
 	return 0;
 }
 
-std::map<std::string, int > logging::register_roles = {
+std::map<std::string, int > logging_options::register_roles = {
 	{"1", DRIVER},
 	{"driver", DRIVER},
 	{"2", PASSENGER},
