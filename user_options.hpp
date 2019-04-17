7c2582089d4021aa0f24b9409ba052694e01ae20
Viacheslav Germonenko <vyacheslav.germonenko@gmail.com>
2019-04-17 14:12:36 +0300

    Non static options
    - now all options classes are non-static
    - option classes stores user data

--- user_options.hpp
+++ user_options.hpp
@@ -4,15 +4,20 @@
 
 typedef class user_options
 {	
+protected:
+	user current_user_;
 public:	
-	static void edit_credentials(const std::string& login);
-	static void change_password(const std::string& login);	
-private:	
-	static credentials ask_new_credentials();
-	static void update(const std::string& SQLtext);
-	static std::string ask_new_password();
+	void edit_credentials();
+	void change_password();	
+protected:	
+	credentials ask_new_credentials() const;
+	void update(const std::string& SQLtext);
+	std::string ask_new_password() const;
 
-	static std::string form_update_credits(const credentials& credits, const std::string& login);
-	static std::string form_update_password(const std::string& password ,const std::string& login);
-	
+	std::string form_update_credits(const credentials& credits) const;
+	std::string form_update_password(const std::string& password) const;
+
+public:
+	user_options(user* current);
+	virtual ~user_options() = default;
 } options;
